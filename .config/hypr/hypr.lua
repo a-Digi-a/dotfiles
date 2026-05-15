2c31537edc999d0a72e61b55a3473601ba4c2e05
a-Digi-a <ShayneM2006@proton.me>
2026-05-15 13:24:41 +0100

    Start lua migration

---
+++ .config/hypr/hypr.lua
@@ -0,0 +1,8 @@
+-- Variables
+
+local terminal = "ghostty"
+local fileManager = "nautilus"
+local menu = "qs -c noctalia-shell ipc call launcher toggle"
+local mainMod = "SUPER"
+
+-- Import Other Files
---
+++ .config/hypr/monitors.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+	output = "eDP-1",
+	mode = "2256x1504@60",
+	position = "0x0",
+	scale = 2,
+})
+
+hl.monitor({
+	output = "DP-1",
+	mode = "1920x1080@144",
+	position = "0x0",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "DP-2",
+	mode = "1920x1080@144",
+	position = "1920x0",
+	scale = 1,
+})
