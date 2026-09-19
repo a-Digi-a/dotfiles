94a53c97b1937be1a8ad53ff811e194d08150242
a-Digi-a <ShayneM2006@proton.me>
2026-09-19 21:00:04 +0100

    fix conform.nvim

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -20,22 +20,22 @@ return {
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				-- local disable_filetypes = {  }
-				-- if disable_filetypes[vim.bo[bufnr].filetype] then
-					-- return nil
-				-- else
+				local disable_filetypes = { null }
+				if disable_filetypes[vim.bo[bufnr].filetype] then
+					return nil
+				else
 					return {
 						timeout_ms = 500,
 						lsp_format = "fallback",
 					}
-				-- end
+				end
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
 				python = { "ruff" },
 				rust = { "rustfmt", "rust-analyzer" },
-                c = { "clang-format" },
+				c = { "clang-format" },
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
